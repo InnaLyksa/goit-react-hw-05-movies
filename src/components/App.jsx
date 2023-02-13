d2ac8c2dfd4eb4e6acbd584d6c056c5dbb85774d
InnaLyksa <in.lyksa@gmail.com>
2023-02-13 18:04:58 +0200

    Changes to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar, SearchForm, GalleryImages, Modal, Button } from './index';
@@ -8,143 +8,110 @@ import { Loader } from '../components/Loader/Loader';
 
 import ScrollToTop from 'react-scroll-to-top';
 
-export class App extends Component {
-  state = {
-    query: '',
-    pictureData: [],
-    modalImg: null,
-    status: 'idle',
-    page: 1,
-    showModal: false,
-    IsLoadingMore: false,
-  };
+export function App() {
+  const [query, setQuery] = useState('');
+  const [modalImg, setModalImg] = useState(null);
+  const [pictureData, setPictureData] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [IsLoadingMore, setIsLoadingMore] = useState(false);
 
-  componentDidUpdate(_, prevProps) {
-    const prevSearch = prevProps.query;
-    const nextSearch = this.state.query;
-    const prevPage = prevProps.page;
-    const nextPage = this.state.page;
-
-    if (prevSearch !== nextSearch || nextPage !== prevPage) {
-      this.loadPicture();
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
+    setStatus('pending');
 
-  loadPicture = () => {
-    const { query, page } = this.state;
-    this.setState({ status: 'pending' });
     api
       .pixabayApi(query, page)
       .then(res => {
         if (res.data.hits.length === 0) {
           toast.error(`There is no "${query}" images.`);
-          this.setState({ status: 'idle' });
+          setStatus('idle');
           return;
         }
-        this.setState(prevState => ({
-          pictureData: [
-            ...prevState.pictureData,
-            ...this.mapper(res.data.hits),
-          ],
-          status: 'resolved',
-          IsLoadingMore:
-            prevState.pictureData.length + res.data.hits.length ===
-            res.data.totalHits
-              ? false
-              : true,
-        }));
+        setPictureData(pictureData => [
+          ...pictureData,
+          ...mapper(res.data.hits),
+        ]);
+        setStatus('resolved');
 
+        const lengthData = (page - 1) * 12 + res.data.hits.length;
+
+        if (lengthData >= res.data.totalHits) {
+          setIsLoadingMore(false);
+        } else {
+          setIsLoadingMore(true);
+        }
         if (res.data.hits.length < 12 && res.data.hits.length > 0) {
           toast.info('There is no more images');
         }
       })
       .catch(() => toast.error(`Ups! Something is wrong :(  Try again later!`));
-  };
+  }, [query, page]);
 
-  mapper = array => {
+  const mapper = array => {
     return array.map(({ id, webformatURL, largeImageURL }) => ({
       id,
       webformatURL,
       largeImageURL,
     }));
   };
-
-  resetPage() {
-    this.setState({
-      page: 1,
-    });
-  }
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
 
-  handleOpenModal = modalImg => {
-    this.toggleModal();
-    this.setState({ modalImg: modalImg });
+  const handleOpenModal = modalImg => {
+    toggleModal();
+    setModalImg(modalImg);
   };
 
-  handleSearchSubmit = searchQuery => {
-    this.setState({
-      query: searchQuery,
-      page: 1,
-      pictureData: [],
-      status: 'idle',
-      modalImg: null,
-      showModal: false,
-      IsLoadingMore: false,
-    });
+  const handleSearchSubmit = searchQuery => {
+    setQuery(searchQuery);
+    setPage(1);
+    setPictureData('');
+    setStatus('idle');
+    setModalImg(null);
+    setShowModal(false);
+    setIsLoadingMore(false);
   };
 
-  onClickloadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const onClickloadMore = () => {
+    setPage(page => page + 1);
   };
 
-  render() {
-    const { pictureData, modalImg, IsLoadingMore, showModal, status } =
-      this.state;
-    const {
-      handleSearchSubmit,
-      onClickloadMore,
-      toggleModal,
-      handleOpenModal,
-    } = this;
-    return (
-      <div>
-        <Searchbar>
-          <SearchForm submitForm={handleSearchSubmit} />
-        </Searchbar>
-        {showModal && <Modal onClose={toggleModal} image={modalImg} />}
+  return (
+    <div>
+      <Searchbar>
+        <SearchForm submitForm={handleSearchSubmit} />
+      </Searchbar>
+      {showModal && <Modal onClose={toggleModal} image={modalImg} />}
 
-        {pictureData.length > 0 && (
-          <GalleryImages
-            pictureData={pictureData}
-            onClick={handleOpenModal}
-          ></GalleryImages>
-        )}
+      {pictureData.length > 0 && (
+        <GalleryImages
+          pictureData={pictureData}
+          onClick={handleOpenModal}
+        ></GalleryImages>
+      )}
 
-        <ScrollToTop
-          color="black"
-          smooth
-          width="30"
-          height="30"
-          style={{
-            background: '#088ecc',
-          }}
-        />
+      <ScrollToTop
+        color="black"
+        smooth
+        width="30"
+        height="30"
+        style={{
+          background: '#088ecc',
+        }}
+      />
 
-        {status === 'pending' && <Loader />}
-        {IsLoadingMore && <Button onClick={onClickloadMore}>Load more</Button>}
-        <ToastContainer
-          autoClose={1000}
-          theme="colored"
-          position="bottom-center"
-        />
-      </div>
-    );
-  }
+      {status === 'pending' && <Loader />}
+      {IsLoadingMore && <Button onClick={onClickloadMore}>Load more</Button>}
+      <ToastContainer
+        autoClose={1000}
+        theme="colored"
+        position="bottom-center"
+      />
+    </div>
+  );
 }
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,62 +1,45 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { IconButton } from '../IconButton/IconButton';
 import { Form, SearchInput } from './SearchForm.styled';
 import { ReactComponent as LoupeIcon } from '../../icons/loupe.svg';
 
-export class SearchForm extends Component {
-  state = {
-    searchQuery: '',
+export function SearchForm({ submitForm }) {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleOnChange = ({ target: { value } }) => {
+    setSearchQuery(value.toLowerCase());
   };
 
-  handleOnChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  handleOnSubmit = e => {
+  const handleOnSubmit = e => {
     e.preventDefault();
-    const { submitForm } = this.props;
-    const { searchQuery } = this.state;
 
     if (searchQuery.trim() === '') {
       toast.warn('Warning! Search must not be empty');
 
       return;
     }
-    submitForm(searchQuery.toLowerCase());
-
-    this.resetForm();
+    submitForm(searchQuery);
+    setSearchQuery('');
   };
 
-  resetForm = () => {
-    this.setState({ searchQuery: '' });
-  };
-
-  render() {
-    const {
-      state: { searchQuery },
-      handleOnChange,
-      handleOnSubmit,
-    } = this;
-
-    return (
-      <Form onSubmit={handleOnSubmit}>
-        <SearchInput
-          type="text"
-          autoComplete="off"
-          placeholder="Search images..."
-          name="searchQuery"
-          autoFocus
-          value={searchQuery}
-          onChange={handleOnChange}
-        />
-        <IconButton type="submit" aria-label="search images">
-          <LoupeIcon width="40" height="40" />
-        </IconButton>
-      </Form>
-    );
-  }
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <SearchInput
+        type="text"
+        autoComplete="off"
+        placeholder="Search images..."
+        name="searchQuery"
+        autoFocus
+        value={searchQuery}
+        onChange={handleOnChange}
+      />
+      <IconButton type="submit" aria-label="search images">
+        <LoupeIcon width="40" height="40" />
+      </IconButton>
+    </Form>
+  );
 }
 
 SearchForm.propTypes = {
