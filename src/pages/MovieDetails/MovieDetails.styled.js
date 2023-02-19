da1bcf7757391f0d8f39effe1f0fa389da68999b
InnaLyksa <in.lyksa@gmail.com>
2023-02-19 17:27:54 +0200

    Update MovieDetails.styled.js

--- src/pages/MovieDetails/MovieDetails.styled.js
+++ src/pages/MovieDetails/MovieDetails.styled.js
@@ -13,7 +13,9 @@ export const GoBackBtn = styled.button`
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   cursor: pointer;
-
+  svg {
+    transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
   &:hover {
     background-color: orange;
     color: #ffff;
