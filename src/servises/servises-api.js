ed82916c4108f722d5e6288b7d67b00e9831344d
InnaLyksa <in.lyksa@gmail.com>
2023-02-19 17:20:40 +0200

    Update servises-api.js

--- src/servises/servises-api.js
+++ src/servises/servises-api.js
@@ -1,40 +1,59 @@
 import axios from 'axios';
 
 export async function getTrendingMovies() {
-  const response = await axios.get(
-    'https://api.themoviedb.org/3/trending/movie/day?api_key=48b510c2367bfa7530de62f6d3fdcc31'
-  );
-
-  return response.data.results;
+  try {
+    const response = await axios.get(
+      'https://api.themoviedb.org/3/trending/movie/day?api_key=48b510c2367bfa7530de62f6d3fdcc31'
+    );
+    return response.data.results;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function SearchMoviesFetch(input) {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?query=${input}&api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US&page=1&include_adult=false`
-  );
-  return response.data.results;
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?query=${input}&api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US&page=1&include_adult=false`
+    );
+    return response.data.results;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function MovieDetailsFetch(id) {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US`
-  );
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US`
+    );
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function MovieCreditsFetch(id) {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US`
-  );
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}/credits?api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US`
+    );
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 export async function MovieReviewsFetch(id) {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US&page=1`
-  );
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=48b510c2367bfa7530de62f6d3fdcc31&language=en-US&page=1`
+    );
 
-  return response.data.results;
+    return response.data.results;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
